6a5e0cb512afb36945ca0d6d95bcaa801a9d9c8c
simran-22 <simranpreet.3d@gmail.com>
2024-09-11 18:00:25 +0530

    Remove firebaf

--- src/main.jsx
+++ src/main.jsx
@@ -8,28 +8,31 @@ import Dashboard from "./components/Dashboard";
 import TvScreen from "./components/TvScreen";
 import SignUp from "./components/SignUp";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "signin",
-    element: <SignIn />,
-  },
-  {
-    path: "dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "tvscreen",
-    element: <TvScreen />,
-  },
-  {
-    path: "signup",
-    element: <SignUp />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "signin",
+      element: <SignIn />,
+    },
+    {
+      path: "dashboard",
+      element: <Dashboard />,
+    },
+    {
+      path: "tvscreen",
+      element: <TvScreen />,
+    },
+    {
+      path: "signup",
+      element: <SignUp />,
+    },
+  ],
+  { basename: "/netflixApp" }
+);
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
